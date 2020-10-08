da8a43df97afea760a84303a322a994af01522fd
System Administrator <root@Rafaels-MacBook-Air.local>
2020-10-07 23:10:22 -0500

    another port

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', router);
 const port = 3000;
+// const port = 8080;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
